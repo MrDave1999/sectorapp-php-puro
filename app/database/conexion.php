83f13ab8c586257f6b96576e7caf72c492bcde6b
MrDave1999 <daveseva2010@hotmail.es>
2021-09-18 13:30:06 -0500

    Added the code that load the .env file

--- app/database/conexion.php
+++ app/database/conexion.php
@@ -1,6 +1,11 @@
 <?php 
 namespace models\conexion;
 
+require_once '../../vendor/autoload.php';
+
+$dotenv = \Dotenv\Dotenv::createImmutable('../../');
+$dotenv->load();
+
 function open()
 {
 	$con = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $_ENV['DB_DATABASE']);
